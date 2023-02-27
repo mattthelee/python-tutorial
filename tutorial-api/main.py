a29a9909580b0d905987cc1d9ae8d103fb90b85e
Corkben <Richeyben@icloud.com>
2023-02-27 10:23:35 +0000

    Update main.py
    Added a branch and created a small fastapi code that reads and displays the time

---
+++ tutorial-api/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+import uvicorn
+from datetime import datetime
+
+now = datetime.now()
+
+current_time = now.strftime("%H:%M:%S")
+print("Current Time =", current_time)
+
+def create_app():
+    app = FastAPI(
+        title="Server",
+        description="training server",
+        version="0.0.1",
+    )
+
+
+    @app.get("/time")
+    async def time():
+        current_time = now.strftime("%H:%M:%S")
+        return {"Current Time =", current_time}
+
+app = create_app()
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info")
