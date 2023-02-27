59d0b035524b5946472d227e4a2aa7984efb5287
Corkben <Richeyben@icloud.com>
2023-02-27 14:15:14 +0000

    Update main.py
    updated and fixed issue with @app.get line

--- tutorial-api/main.py
+++ tutorial-api/main.py
@@ -14,13 +14,13 @@ def create_app():
         version="0.0.1",
     )
 
-
-    @app.get("/time")
-    async def time():
-        current_time = now.strftime("%H:%M:%S")
-        return {"Current Time =", current_time}
-
 app = create_app()
+
+@app.get("/time")
+async def time():
+    current_time = now.strftime("%H:%M:%S")
+    return {"Current Time =", current_time}
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info")
