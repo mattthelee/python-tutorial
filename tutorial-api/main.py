ef6c8afdf0999b5a50829a1695a275256af3c769
liamstainton <liamstainton18@hotmail.co.uk>
2023-03-06 10:48:16 +0000

    added new post and get request

--- tutorial-api/main.py
+++ tutorial-api/main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 import uvicorn
 import time
 from datetime import datetime
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Union
+
 
 now = datetime.now()
 
@@ -13,10 +17,10 @@ def create_app():
     app = FastAPI(
         title="Server",
         description="training server",
-        version="0.0.1",
+        version="0.0.1"
     )
     
-app.get("/say-hello")
+@app.get("/say-hello")
 async def hello():
     return {"message": "Hello World"}
     
@@ -25,6 +29,39 @@ async def time():
     current_time = now.strftime("%H:%M:%S")
     return {"Current Time =", current_time}
 
-app.get("/say-bye")
+@app.get("/say-bye")
 async def goodbye():
     return {"message": "Goodbye World"}
+
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    discount: Union[float, None] = None
+    location: str
+    stock: float
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
+
+async def create_item_discount(item: Item):
+    if item.stock <= 2:
+       new_price = item.price / item.discount
+    return ({"Price with discount": new_price})
+
+
+
+@app.get("/about")
+async def about():
+    return {'''
+
+
+    Welcome to this test server
+    Here you will see we have made an about page...
+    This is the first server we made :)
+    
+    '''
+    }
