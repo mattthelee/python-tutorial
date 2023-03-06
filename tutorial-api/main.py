d9395f347422794074a938e741ff3c604487d24d
ABatt33 <adambattioui@hotmail.com>
2023-03-06 14:11:50 +0000

    new branch

--- tutorial-api/main.py
+++ tutorial-api/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 import uvicorn
 import time
+import random
 from datetime import datetime
+from typing import Union
+from pydantic import BaseModel
 
 now = datetime.now()
 
@@ -15,8 +18,11 @@ def create_app():
         description="training server",
         version="0.0.1",
     )
+    return app
+
 
 app = create_app()
+
 
 @app.get("/time")
 async def time():
@@ -26,3 +32,24 @@ async def time():
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info")
+
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
+
+goodbye_msg = ["Goodbye", "See ya", "Auf Wiedersehen"]
+
+
+@app.get("/website_msg/")
+async def goodbye(goodbye_msg):
+    print(goodbye_msg[random.randint(0, len(goodbye_msg) - 1)])
+    return
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
